af911105346e405cdb948569b921d63c8ca31c9c
David Pfahler <david.pfahler@x-tention.at>
2022-10-03 08:18:44 +0200

    add iced ui

--- src/lib/cli.rs
+++ src/lib/cli.rs
@@ -11,21 +11,3 @@ pub struct Cli {
 pub fn parse() -> Cli {
     Cli::parse()
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_parse() {
-        let help = vec!["--help"];
-        Cli::parse_from(help);
-
-        let path = vec!["--path", "path/to/file.txt"];
-        let out = Cli::parse_from(path);
-        assert_eq!(out.path.to_str(), Some("path/to/file.txt"));
-
-        let empty: Vec<&str> = Vec::new();
-        Cli::parse_from(empty);
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod app;
 mod lib;
+mod ui;
 
 fn main() {
 
     let args = lib::cli::parse();
     let config = lib::config::parse(args.path);
-    println!("config parsed: {}", config.test);
     app::hello();
+    ui::stopwatch::run();
 }
